1c0f6101e49cda62573f6acbea5415fc096b7773
matthews.andrew <matthews.andrew@0aa08b0e-b330-0410-a7dc-93bad6e629e8>
2007-10-02 02:03:30 +0000

    brought up to date on vs 2008b2 &tested on joseki
    git-svn-id: http://linqtordf.googlecode.com/svn/trunk@53 0aa08b0e-b330-0410-a7dc-93bad6e629e8

---
+++ src/unit-testing/unit-tests/JosekiTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinqToRdf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RdfMusic;
+
+namespace RdfSerialisationTest
+{
+    [TestClass]
+    public class JosekiTests
+    {
+        [TestMethod]
+        public void JosekiQueryWithProjection()
+        {
+			TripleStore ts = CreateOnlineTripleStore();
+			IRdfQuery<Track> qry = new RDF(ts).ForType<Track>();
+			var q = from t in qry
+							where t.Year == "2007" &&
+							t.GenreName == "Rory Blyth: The Smartest Man in the World"
+							select new { t.Title, t.FileLocation };
+			foreach (var track in q)
+			{
+				Console.WriteLine(track.Title + ": " + track.FileLocation);
+			}
+        }
+
+        protected TripleStore CreateOnlineTripleStore()
+        {
+            TripleStore ts = new TripleStore();
+            ts.EndpointUri = @"http://localhost:2020/music";
+            ts.QueryType = QueryType.RemoteSparqlStore;
+            return ts;
+        }
+
+    }
+}
