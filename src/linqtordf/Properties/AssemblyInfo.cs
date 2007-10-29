ed6786f321d5c26644336ec0e349094a6e41781d
matthews.andrew <matthews.andrew@0aa08b0e-b330-0410-a7dc-93bad6e629e8>
2007-10-29 01:46:00 +0000

    updated version number to 0.4.0 in preparation for v0.4 release
    git-svn-id: http://linqtordf.googlecode.com/svn/trunk@84 0aa08b0e-b330-0410-a7dc-93bad6e629e8

--- src/linqtordf/Properties/AssemblyInfo.cs
+++ src/linqtordf/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Security.Permissions;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 
 //[assembly: IsolatedStorageFilePermission(SecurityAction.RequestMinimum, UserQuota = 1048576)]
 //[assembly: SecurityPermission(SecurityAction.RequestRefuse, UnmanagedCode = true)]
