ddebe3b639d1663d69499967881fa2b12fc41e49
matthews.andrew <matthews.andrew@0aa08b0e-b330-0410-a7dc-93bad6e629e8>
2007-10-31 06:28:56 +0000

    updated documentation
    git-svn-id: http://linqtordf.googlecode.com/svn/trunk@87 0aa08b0e-b330-0410-a7dc-93bad6e629e8

--- doc/Samples/SystemScanner/src/Core/SystemScannerModel.rdfx.entities.cs
+++ doc/Samples/SystemScanner/src/Core/SystemScannerModel.rdfx.entities.cs
@@ -2,32 +2,36 @@
 using LinqToRdf;
 using System.Data.Linq;
 
-namespace SystemScannerModel
+namespace SystemScannerModel 
 {
 
-    [OwlResource(OntologyName = "$fileinputname$", RelativeUriReference = "Artifact")]
-    public partial class Artifact : OwlInstanceSupertype
-    {
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "artifactExists")]
-        public bool ArtifactExists { get; set; }
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "dateCreated")]
-        public DateTime DateCreated { get; set; }
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "dateLastModified")]
-        public DateTime DateLastModified { get; set; }
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "filePath")]
-        public string FilePath { get; set; }
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "isReadOnly")]
-        public bool IsReadOnly { get; set; }
-    }
+[OwlResource(OntologyName="$fileinputname$", RelativeUriReference="Artifact")]
+public partial class Artifact : OwlInstanceSupertype
+{
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="artifactExists")]
+		public bool ArtifactExists{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="dateCreated")]
+		public DateTime DateCreated{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="dateLastModified")]
+		public DateTime DateLastModified{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="filePath")]
+		public string FilePath{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="isReadOnly")]
+		public bool IsReadOnly{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="hasACL")]
+		public string HasACL{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="hasDependencyOn")]
+		public EntitySet<Artifact> HasDependencyOn{get;set;}
+}
 
-    [OwlResource(OntologyName = "$fileinputname$", RelativeUriReference = "assembly")]
-    public partial class Assembly : Artifact
-    {
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "isSigned")]
-        public bool IsSigned { get; set; }
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "strongKey")]
-        public string StrongKey { get; set; }
-        [OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference = "version")]
-        public string Version { get; set; }
-    }
+[OwlResource(OntologyName="$fileinputname$", RelativeUriReference="assembly")]
+public partial class Assembly : Artifact
+{
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="isSigned")]
+		public bool IsSigned{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="strongKey")]
+		public string StrongKey{get;set;}
+		[OwlResource(OntologyName = "SystemScannerModel ", RelativeUriReference="version")]
+		public string Version{get;set;}
+}
 }
