48aed249a1c7d36f0772f79948adc443e44e954a
matthews.andrew <matthews.andrew@0aa08b0e-b330-0410-a7dc-93bad6e629e8>
2008-04-28 11:13:13 +0000

    git-svn-id: http://linqtordf.googlecode.com/svn/trunk@93 0aa08b0e-b330-0410-a7dc-93bad6e629e8

---
+++ src/linqtordf/FunctionalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linqtordf
+{
+    public static class FunctionalExtensions
+    {
+        public static IEnumerable<R> Map<T, R>(this IEnumerable<T> seq, Func<T, R> f)
+        {
+            foreach (T t in seq)
+                yield return f(t);
+        }
+        public static Func<T, T> On<T>(this Func<T, T> inner, Func<T, T> outer)
+        {
+            return t => outer(inner(t));
+        }
+    }
+
+}
